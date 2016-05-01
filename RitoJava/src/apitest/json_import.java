b28b4a1f929b22641083ed63b3bf846d36295200
restonpiston <felixlagunateno@yahoo.es>
2016-05-01 17:36:53 +0200

    AA

--- RitoJava/src/apitest/json_import.java
+++ RitoJava/src/apitest/json_import.java
@@ -9,6 +9,7 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,16 +39,28 @@ public class json_import {
 
   public static void main(String[] args) throws IOException, JSONException {
 	int id=0;
-	String nombre="restonpiston";
+	String nombre="sergyonyx";
 	String webs="https://euw.api.pvp.net/api/lol/euw";
-	String Api="fcb02306-537c-497a-8704-b804c1e73d1e";
-    JSONObject summ = readJsonFromUrl(webs+"/v1.4/summoner/by-name/"+nombre+"?api_key="+Api);
-    System.out.println(summ.toString());
+	String Api="?api_key=fcb02306-537c-497a-8704-b804c1e73d1e";
+    JSONObject summ = readJsonFromUrl(webs+"/v1.4/summoner/by-name/"+nombre+Api);
+    //System.out.println(summ.toString());
     JSONObject yo=(JSONObject) summ.get(nombre);
     summ=null;
     id=(int) yo.get("id");
     yo=null;
-    JSONObject suma=readJsonFromUrl(webs+"/v1.3/stats/by-summoner/"+id+"/summary");
-    
+    JSONObject suma=readJsonFromUrl(webs+"/v1.3/stats/by-summoner/"+id+"/summary"+Api);
+    JSONArray suma2;
+    suma2=suma.getJSONArray("playerStatSummaries");
+    for (int i=0;i<suma2.length();++i){
+    	JSONObject o=suma2.getJSONObject(i);
+    	System.out.print("Tipo: "+o.get("playerStatSummaryType"));
+    	System.out.print(", Victorias: "+o.get("wins"));
+    	JSONObject o2=o.getJSONObject("aggregatedStats");
+    	if (o2.keys().hasNext()){
+    		
+    	}
+    	System.out.print(o2.toString());
+    	System.out.println("");
+    }
   }
 }
